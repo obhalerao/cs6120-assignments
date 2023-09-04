8e28f9cae890101ef1d249d9e40abf946fb6c9a7
Omkar Bhalerao <opb7@cornell.edu>
2023-09-04 14:31:50 -0400

    refactor rapidjson with nlohmann/json

--- lesson2/count_branches.cpp
+++ lesson2/count_branches.cpp
@@ -1,42 +1,25 @@
-#include "rapidjson/document.h"
+#include "json.hpp"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-using namespace rapidjson;
+using json = nlohmann::json;
 
 // Counts the number of branches in the JSON representation of a bril program.
 
 int main(int argc, char* argv[]){
 
-  // expects json filename as argument
-  const char* infile = argv[1];
-  std::ifstream fin(infile);
-  std::stringstream buf;
-  buf << fin.rdbuf();
-  std::string json = buf.str();
-  fin.close();
-
-  // parses text of file into json
-  Document document;
-  document.Parse(json.c_str());
-
-  const Value& functions = document["functions"];
+  // pipes in json as input
+  json data;
+  std::cin >> data;
 
   int branches {0};
 
-  // iterate through functions and instrs
-  for(Value::ConstValueIterator function = functions.Begin();
-      function != functions.End(); function++){
-        const Value& instrs = (*function)["instrs"];
-        for(Value::ConstValueIterator instr = instrs.Begin();
-            instr != instrs.End(); instr++){
-            if(instr->HasMember("op") && (*instr)["op"] == "br") branches++;
-        }
+  for(auto function: data["functions"]){
+    for(auto instr: function["instrs"]){
+      if(instr.contains("op") && instr["op"] == "br") branches++;
+    }
   }
 
-  //print number of branches
   std::cout << "This program has " << branches << " branches." << std::endl;
-       
-
 }
