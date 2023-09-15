2329db5e29211368e1365c5b3e7cb02d18a0d9ee
Omkar Bhalerao <opb7@cornell.edu>
2023-09-14 23:05:18 -0400

    finally deal with undefined vars

--- lesson4/main.cpp
+++ lesson4/main.cpp
@@ -59,9 +59,9 @@ public:
     }
 };
 
-class ConstantPropAnalysis: DataflowBaseSingleton<std::unordered_map<std::string, std::pair<ll, type_t>>>{
+class ConstantPropAnalysis: DataflowBaseSingleton<std::unordered_map<std::string, std::pair<std::optional<ll>, type_t>>>{
 public:
-    using varmap_t = std::unordered_map<std::string, std::pair<ll, type_t>>;
+    using varmap_t = std::unordered_map<std::string, std::pair<std::optional<ll>, type_t>>;
     
     varmap_t makeTop() override {
         return {};
@@ -73,7 +73,7 @@ public:
             for(auto it: inConsts){
                 if(unionMap.find(it.first) != unionMap.end() 
                     && it.second != unionMap[it.first]){
-                        unionMap.erase(it.first);
+                        unionMap[it.first] = std::make_pair(std::nullopt, type_t::b_int);
                 }else if(unionMap.find(it.first) == unionMap.end()){
                     unionMap[it.first] = it.second;
                 }
@@ -93,15 +93,16 @@ public:
         if(node.instr.contains("dest")){
             if(node.instr["op"] == "const" && (node.instr["type"] == "int"
                 || node.instr["type"] == "bool")){
+                std::cout << "hi" << std::endl;
                 ll val = node.instr["value"];
                 type_t type = str2type[node.instr["type"]];
-                outMap[node.instr["dest"]] = std::make_pair(val, type);
+                outMap[node.instr["dest"]] = std::make_pair(std::optional<ll>(val), type);
             }else if(node.instr["op"] == "id" && outMap.find(node.instr["args"][0]) != outMap.end()){
                 outMap[node.instr["dest"]] = outMap[node.instr["args"][0]];
+            }else if(node.instr["type"].type_name() == "string" && str2type.find(node.instr["type"]) != str2type.end()){
+                outMap[node.instr["dest"]] = std::make_pair(std::nullopt, str2type[node.instr["type"]]);
             }
         }
-
-        
 
         return {outMap, outMap == old};
     }
@@ -109,8 +110,11 @@ public:
     std::string stringify(varmap_t t) override {
         std::vector<std::string> pairs;
         std::transform(t.begin(), t.end(), std::back_inserter(pairs), 
-        [](std::pair<std::string, std::pair<ll, type_t>> entry){
-            return "(" + entry.first + " -\\> (" + std::to_string(entry.second.first) + ", " + type2str[entry.second.second] + "))";
+        [](std::pair<std::string, std::pair<std::optional<ll>, type_t>> entry){
+            if(!entry.second.first.has_value()){
+                return "(" + entry.first + " -\\> (" + "UNDEF" + ", " + type2str[entry.second.second] + "))"; 
+            }
+            return "(" + entry.first + " -\\> (" + std::to_string(entry.second.first.value_or(0)) + ", " + type2str[entry.second.second] + "))";
         });
         std::sort(pairs.begin(), pairs.end());
         return joinToString<typename std::vector<std::string>::iterator>(pairs.begin(), pairs.end(), "\\{", ", ", "\\}");
@@ -154,7 +158,7 @@ int main(int argc, char* argv[]) {
         // DataFlowAnalysisBase analyzer;
         bool forwards;
         if (analysis == "cprop") {
-            auto analyzer = DataFlowAnalysis<ConstantPropAnalysis, std::unordered_map<std::string, std::pair<ll, type_t>>>(&cfg);
+            auto analyzer = DataFlowAnalysis<ConstantPropAnalysis, std::unordered_map<std::string, std::pair<std::optional<ll>, type_t>>>(&cfg);
             forwards = true;
             if (smartMode) {
                 analyzer.smartAnalyze(forwards);
