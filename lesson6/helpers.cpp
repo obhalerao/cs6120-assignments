39e829104feed92fd711b5f094b5e2c9f8113fd0
Omkar Bhalerao <opb7@cornell.edu>
2023-09-27 01:46:31 -0400

    tried hard on converting to ssa; still failing for unknown reasons things broken: phi nodes not having values updated, somehow new cfg has more blocks than old

--- lesson6/helpers.cpp
+++ lesson6/helpers.cpp
@@ -4,151 +4,32 @@
 #include <unordered_set>
 using json = nlohmann::json;
 
-// created by ChatGPT, https://chat.openai.com/share/f727676a-4d04-47f7-8924-53678fbec5c0
-class PrefixCounter {
-public:
-    PrefixCounter(const std::string& prefix) : prefix(prefix), count(0) {}
-
-    std::string generate() {
-        std::string result = prefix + "_" + std::to_string(count);
-        count++;
-        return result;
-    }
-
-private:
-    std::string prefix;
-    int count;
-};
-
-// originally created by ChatGPT, https://chat.openai.com/share/f727676a-4d04-47f7-8924-53678fbec5c0
-template <typename IteratorT> std::string joinToString(IteratorT begin,
-                         IteratorT end,
-                         const std::string& prefix,
-                         const std::string& separator,
-                         const std::string& suffix) {
-    if (begin == end) {
-        return prefix + suffix;
-    }
-
-    std::ostringstream oss;
-    oss << prefix;
-    oss << *begin;  // Add the first element
-    ++begin;
-
-    while (begin != end) {
-        oss << separator << *begin;  // Add separator and the next element
-        ++begin;
-    }
-    oss << suffix;
-
-    return oss.str();
-}
-
-// from https://stackoverflow.com/a/26221725
-template<typename ... Args>
-std::string string_format(const std::string& format, Args ... args)
-{
-    int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
-    auto size = static_cast<size_t>( size_s );
-    std::unique_ptr<char[]> buf( new char[ size ] );
-    std::snprintf( buf.get(), size, format.c_str(), args ... );
-    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
-}
-
-std::string lower(std::string data) {
-    std::string ret = std::string(data);
-    std::transform(ret.begin(), ret.end(), ret.begin(), [](unsigned char c){ return std::tolower(c); });
-    return ret;
-}
-
-// from briltxt.py
-std::string value_to_string(json type, json value) {
-    const std::unordered_map<int, std::string> control_chars {
-        {0, std::string("\\0")},
-        {7, std::string("\\a")},
-        {8, std::string("\\b")},
-        {9, std::string("\\t")},
-        {10, std::string("\\n")},
-        {11, std::string("\\v")},
-        {12, std::string("\\f")},
-        {13, std::string("\\r")},
-    };
-
-    if (!type.is_object()) {
-        auto type_str = std::string(type);
-        std::string lowered = lower(type_str);
-        if (lowered == "char") {
-            std::string value_str = value.get<std::string>();
-            uint32_t value_int = value_str.at(0);
-            auto search = control_chars.find(value_int);
-            if (search != control_chars.end()) {
-                std::string ret = std::string("'") + search->second + std::string("'");
-                return ret;
+std::string short_unique_prefix(std::set<std::string> strs){
+    bool bad = true;
+    std::vector<char> prefix;
+    while(bad){
+        bad = false;
+        prefix.push_back('a');
+        for(auto str: strs){
+            bool good = false;
+            for(int i = 0; i < prefix.size(); i++){
+                if(i >= str.length()|| prefix[i] != str[i]){
+                    good = true;
+                    break;
+                }
+            }
+            if(!good){
+                if(prefix[prefix.size()-1] == 'z'){
+                    bad = true;
+                    break;
+                }else{
+                    prefix[prefix.size()-1]++;
+                }
             }
         }
     }
-
-    std::string ret;
-
-    if (value.is_number_integer()) {
-        ret = std::to_string(value.get<int64_t>());
-    } else if (value.is_boolean()) {
-        ret = value.get<bool>() ? "true" : "false";
-    } else if (value.is_number_float()) {
-        ret = std::to_string(value.get<float>());
-    }
-    return ret;
+    std::string ans(prefix.begin(), prefix.end());
+    return ans;
 }
 
-// from briltxt.py
-std::string type_to_string(json type) {
-    if (type.is_object()) {
-        for (auto& el : type.items()) {
-            return std::string(el.key()) + "<" + type_to_string(el.value()) + ">";
-        }
-        return "type_to_string_failed";
-    }
-    else {
-        return type;
-    }
-}
 
-// from briltxt.py
-std::string instr_to_string(json instr) {
-    if (instr.contains("op") && instr["op"] == "const") {
-        std::string dest_str = instr["dest"];
-        std::string tyann = (instr.contains("type")) ? (": " + type_to_string(instr["type"])) : "";
-        
-
-        auto dest = instr["dest"].get<std::string>();
-        std::string value = value_to_string(instr["type"], instr["value"]);
-
-        return dest + tyann + " = const " + value;
-    }
-    else {
-        std::string rhs = instr["op"];
-        if (instr.contains("funcs")) {
-            for (auto func : instr["funcs"]) {
-                rhs += " @" + func.get<std::string>();
-            }
-        }
-        if (instr.contains("args")) {
-            for (auto arg : instr["args"]) {
-                rhs += " " + arg.get<std::string>();
-            }
-        }
-        if (instr.contains("labels")) {
-            for (auto label : instr["labels"]) {
-                rhs += " ." + label.get<std::string>();
-            }
-        }
-        if (instr.contains("dest")) {
-            std::string tyann = (instr.contains("type")) ? (": " + type_to_string(instr["type"])) : "";
-            auto dest = instr["dest"].get<std::string>();
-            return dest + tyann + " = " + rhs;
-        } else {
-            return rhs;
-        }
-    }
-}
--- lesson6/main.cpp
+++ lesson6/main.cpp
@@ -1,9 +1,154 @@
 #include <iostream>
+#include <stack>
 
 #include "dominator_analysis_temp.hpp"
+#include "helpers.cpp"
 
 #include "../src/lib/dataflow_analysis.hpp"
 #include "../src/lib/cfg.hpp"
+#include "../src/lib/cfg_utils.hpp"
+
+std::unordered_map<std::string, std::set<int>> get_defs(CFG &cfg){
+  std::unordered_map<std::string, std::set<int>> defs_map;
+  int idx = 0;
+  for(auto block: cfg.blocks){
+    for(auto node: block.nodes){
+      if(cfg.nodes[node].instr.contains("dest")){
+        if(defs_map.find(cfg.nodes[node].instr["dest"]) == defs_map.end()){
+          defs_map[cfg.nodes[node].instr["dest"]] = {};
+        }
+        defs_map[cfg.nodes[node].instr["dest"]].insert(idx);
+      }
+    }
+    idx++;
+  }
+  return defs_map;
+}
+
+std::vector<std::set<std::string>> get_phi_defs(CFG &cfg, DominatorAnalysis &analyzer,
+  std::unordered_map<std::string, std::set<int>> &defs_map){
+    std::vector<std::set<std::string>> phi_defs;
+    for(int i = 0; i < cfg.blocks.size(); i++) phi_defs.push_back({});
+    for(auto &[var, blocks]: defs_map){
+      bool bad = true;
+      std::set<int> to_process(blocks);
+      while(bad){
+        bad = false;
+        std::set<int> to_add;
+        for(int cur_block: to_process){
+          for(int blk: analyzer.frontier[cur_block]){
+            phi_defs[blk].insert(var);
+            if(blocks.find(blk) != blocks.end()){
+              bad = true;
+              blocks.insert(blk);
+              to_add.insert(blk);
+            }
+          }
+        }
+        to_process = to_add;
+      }
+    }
+  return phi_defs;
+}
+
+json add_phi_nodes(CFG &cfg, DominatorAnalysis &analyzer){
+  std::unordered_map<std::string, std::set<int>> defs_map = get_defs(cfg);
+  std::vector<std::set<std::string>> phi_defs = get_phi_defs(cfg, analyzer, defs_map);
+  json instrs;
+  for(auto blk: cfg.blocks){
+    json label;
+    label["label"] = blk.blockName;
+    instrs.push_back(label);
+    for(auto var: phi_defs[blk.id]){
+      json phi_instr = json{{"op", "phi"}};
+      phi_instr["dest"] = var;
+      instrs.push_back(phi_instr);
+    }
+    for(auto node: blk.nodes){
+      instrs.push_back(cfg.nodes[node].instr);
+    }
+  }
+  return instrs;
+}
+
+void relabel_block(CFG &cfg, DominatorAnalysis &analyzer, int block_id, int parent_id,
+  std::map<std::string, std::stack<std::string>> &variable_names,
+  std::map<std::string, PrefixCounter*> &counters){
+  std::map<std::string, int> totals;
+  for(auto node_id: cfg.blocks[block_id].nodes){
+    CFGNode cur_node = cfg.nodes[node_id];
+    if(cur_node.instr.contains("op") && cur_node.instr["op"] == "phi"){
+      continue;
+    }
+    if(cur_node.instr.contains("args")){
+      json new_args;
+      for(std::string arg: cur_node.instr["args"]){
+        std::string new_name = variable_names[arg].top();
+        new_args.push_back(new_name);
+      }
+      cur_node.instr["args"] = new_args;
+    }
+    if(cur_node.instr.contains("dest")){
+      std::string old_dest = cur_node.instr["dest"];
+      std::string new_dest = counters[old_dest]->generate();
+      variable_names[old_dest].push(new_dest);
+      if(totals.find(old_dest) == totals.end()) totals[old_dest] = 1;
+      else totals[old_dest]++;
+    }
+  }
+
+  for(auto succ: cfg.blocks[block_id].succs){
+    for(auto node_id: cfg.blocks[block_id].nodes){
+      CFGNode cur_node = cfg.nodes[node_id];
+      if(!(cur_node.instr.contains("op") && cur_node.instr["op"] == "phi")){
+        break;
+      }
+      cur_node.instr["args"].push_back(variable_names[cur_node.instr["dest"]].top());
+      cur_node.instr["labels"].push_back(cfg.blocks[block_id].blockName);
+    }
+  }
+
+  for(auto child: analyzer.dominator_tree[block_id]){
+    if(child == parent_id) continue;
+    relabel_block(cfg, analyzer, child, block_id, variable_names, counters);
+  }
+
+  for(auto entry: totals){
+    for(int i = 0; i < entry.second; i++) variable_names[entry.first].pop();
+  }
+}
+
+std::set<std::string> get_vars(json instrs){
+  std::set<std::string> ans;
+  for(auto instr: instrs){
+    if(instr.contains("dest")){
+      ans.insert(instr["dest"]);
+    }
+    if(instr.contains("args")){
+      for(std::string arg: instr["args"]){
+        ans.insert(arg);
+      }
+    }
+  }
+  return ans;
+}
+
+void relabel_vars(CFG &cfg, DominatorAnalysis &analyzer, std::set<std::string> vars){
+  std::map<std::string, std::stack<std::string>> variable_names;
+  std::map<std::string, PrefixCounter*> counters;
+  std::string prefix = short_unique_prefix(vars);
+  for(auto var: vars){
+    std::stack<std::string> curstk;
+    curstk.push(var);
+    variable_names[var] = curstk;
+    PrefixCounter* pc = new PrefixCounter(prefix + "_" + var);
+    counters[var] = pc;
+  }
+  relabel_block(cfg, analyzer, 0, -1, variable_names, counters);
+  for(auto var: vars){
+    delete counters[var];
+  }
+}
 
 int main(int argc, char* argv[]){
   json prog;
@@ -24,15 +169,13 @@ int main(int argc, char* argv[]){
   for(auto func : prog["functions"]){
         CFG cfg(func["name"].get<std::string>(), func["instrs"]);
         auto analyzer = DominatorAnalysis(&cfg);
-        if(naiveMode){
-            analyzer.naiveAnalyze();
-        }else{
-            analyzer.smartAnalyze();
-        }
-        printf("%s\n", analyzer.report().c_str());
-        printf("%s\n", analyzer.report_natural_loops().c_str());
-        if (profileMode) {
-            fprintf(stderr, "Total function calls for function %s when computing dominators: %d\n", func["name"].get<std::string>().c_str(), analyzer.count);
-        }
+        analyzer.smartAnalyze();
+        json new_instrs = add_phi_nodes(cfg, analyzer);
+        CFG new_cfg(func["name"].get<std::string>(), new_instrs);
+        auto new_analyzer = DominatorAnalysis(&new_cfg);
+        new_analyzer.smartAnalyze();
+        std::set<std::string> vars = get_vars(new_instrs);
+        relabel_vars(new_cfg, new_analyzer, vars);
+        std::cout << new_cfg.toInstrs() << std::endl;
     }
 }
