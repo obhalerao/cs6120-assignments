6d3a5b1c1a4789a0d6a763cd9311d331cf5a12ca
Sanjit Basker <sb866@cornell.edu>
2023-10-02 12:11:58 -0400

    more WIP

--- lesson7/basic_loop_pass.cpp
+++ lesson7/basic_loop_pass.cpp
@@ -7,17 +7,21 @@ using namespace llvm;
 namespace {
 
     struct LessDumbLoopPass : public PassInfoMixin<LessDumbLoopPass> {
+        static bool isRequired() { return true; }
+
         PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
             int32_t fun_counter = 0;
 
-            for (auto &F: M) {
+            LLVMContext &Ctx = M.getContext();
+            std::vector<Type *> paramTypes = {Type::getInt32Ty(Ctx), Type::getInt32Ty(Ctx)};
+            Type *retType = Type::getVoidTy(Ctx);
+            FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
+            FunctionCallee logFunc =
+                M.getOrInsertFunction("logop", logFuncType);
 
-                LLVMContext &Ctx = F.getContext();
-                std::vector<Type *> paramTypes = {Type::getInt32Ty(Ctx)};
-                Type *retType = Type::getVoidTy(Ctx);
-                FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
-                FunctionCallee logFunc =
-                        F.getParent()->getOrInsertFunction("logop", logFuncType);
+
+            for (auto &F: M.functions()) {
+
                 FunctionAnalysisManager &FAM = AM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
                 LoopInfo &LI = FAM.getResult<LoopAnalysis>(F);
 
@@ -25,19 +29,19 @@ namespace {
                 for (auto &L: LI) {
                     errs() << L->getName() << "\n";
 
-                    auto header = L->getHeader();
-                    IRBuilder<> builder(&(header->front()));
-                    builder.SetInsertPoint(&(header->front()));
+                    // auto header = L->getHeader();
+                    // IRBuilder<> builder(&(header->front()));
+                    // builder.SetInsertPoint(&(header->front()));
 
-                    auto arg1 = llvm::ConstantInt::get(Ctx, llvm::APInt(32, fun_counter, true));
-                    auto arg2 = llvm::ConstantInt::get(Ctx, llvm::APInt(32, loop_counter, true));
-                    Value* args[] = {arg1, arg2};
-                    builder.CreateCall(logFunc, args);
+                    // auto arg1 = llvm::ConstantInt::get(Ctx, llvm::APInt(32, fun_counter, true));
+                    // auto arg2 = llvm::ConstantInt::get(Ctx, llvm::APInt(32, loop_counter, true));
+                    // Value* args[] = {arg1, arg2};
+                    // builder.CreateCall(logFunc, args);
                     loop_counter++;
                 }
                 fun_counter++;
             }
-            return PreservedAnalyses::all();
+            return PreservedAnalyses::none();
         };
     };
 
@@ -50,7 +54,7 @@ llvmGetPassPluginInfo() {
             .PluginName = "Skeleton pass",
             .PluginVersion = "v0.1",
             .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
-                PB.registerPipelineStartEPCallback(
+                PB.registerOptimizerLastEPCallback(
                         [](ModulePassManager &MPM, OptimizationLevel Level) {
                             MPM.addPass(LessDumbLoopPass());
                         });
