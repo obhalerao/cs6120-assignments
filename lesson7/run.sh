3873a4f01ccc328716d9c37f8faf25834ba33f77
Sanjit Basker <sb866@cornell.edu>
2023-10-02 18:18:48 -0400

    all done

--- lesson7/run.sh
+++ lesson7/run.sh
@@ -1,4 +1,4 @@
-`brew --prefix llvm`/bin/clang -fpass-plugin=`ls ../build/L7Pass.*` -c ../c_benchmarks/something.c -o example.o
+clang-llvm -fpass-plugin=`ls ../build/L7Pass.*` -c ../c_benchmarks/something.c -o example.o
 cc -c rtlib.c -o rtlib.o
 cc example.o rtlib.o
 ./a.out
